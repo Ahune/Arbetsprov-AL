cc0e7c7cefa999b20e179357a6868c503815c5d8
Alexander <Alexander.mounzer@gmail.com>
2022-11-20 04:10:15 +0100

    cleaning IMatchInterface

--- StatsByAlfaLaval.Application/Services/MatchCollection/IMatchCollectionService.cs
+++ StatsByAlfaLaval.Application/Services/MatchCollection/IMatchCollectionService.cs
@@ -4,8 +4,5 @@ namespace StatsByAlfaLaval.Application.Services.MatchCollection;
 
 public interface IMatchCollectionService
 {
-    IList<Match>? CleanByRegex(string rawSource);
-
     List<IList<Match>> CleanByRegex(IEnumerable<string> strResult);
-
 }
--- StatsByAlfaLaval.Application/Services/MatchCollection/MatchCollectionService.cs
+++ StatsByAlfaLaval.Application/Services/MatchCollection/MatchCollectionService.cs
@@ -4,11 +4,6 @@ namespace StatsByAlfaLaval.Application.Services.MatchCollection;
 
 public class MatchCollectionService : IMatchCollectionService
 {
-    public IList<Match> CleanByRegex(string rawSource)
-    {
-        IList<Match> r = Regex.Matches(rawSource, "\\w+");
-        return r;
-    }
 
     public List<IList<Match>> CleanByRegex(IEnumerable<string> strResult)
     {
